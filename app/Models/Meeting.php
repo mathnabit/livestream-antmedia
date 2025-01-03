762602cfd3470f1fa427ca873fad2d27e42c3960
Omar AIT ABDERRAHMAN <omar.atbd@gmail.com>
2025-01-03 21:59:42 +0100

    feat: set up database schema and models

---
+++ app/Models/Meeting.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Meeting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'stream_url',
+        'total_visitors',
+        'current_viewers',
+    ];
+
+    public function visitors()
+    {
+        return $this->hasMany(Visitor::class);
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+}
