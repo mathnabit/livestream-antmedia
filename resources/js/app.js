54631b9942ac1bc906e94ad85387171559303191
Omar AIT ABDERRAHMAN <omar.atbd@gmail.com>
2025-01-03 14:57:02 +0100

    build: integrate and configure vuejs

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
 import './bootstrap';
+// Import the Vue instance
+import { createApp } from 'vue';
+// Import the HomeComponent
+import HomeComponent from "./components/HomeComponent.vue";
+// Mount the app to the #app element
+const app = createApp({});
+app.component("home-component", HomeComponent);
+app.mount("#app");
