01475d12b217349a3d28e4352a01ad01752d1d8c
Omar AIT ABDERRAHMAN <omar.atbd@gmail.com>
2025-01-03 16:14:14 +0100

    build: integrate and configure vuetify

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,28 @@
 import './bootstrap';
 // Import the Vue instance
 import { createApp } from 'vue';
+
+// Import the Vuetify instance
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 // Import the HomeComponent
 import HomeComponent from "./components/HomeComponent.vue";
-// Mount the app to the #app element
+
+// Create a new Vuetify instance
+const vuetify = createVuetify({
+    components,
+    directives,
+})
+
+// Create a new Vue instance
 const app = createApp({});
+
+// Register the HomeComponent component
 app.component("home-component", HomeComponent);
-app.mount("#app");
+
+// Mount the app
+app.use(vuetify).mount("#app");
+
